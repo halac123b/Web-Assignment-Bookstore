a680f34feb51d92056b6f8a07726549b1cc0427c
lvquang <quang.le696969@hcmut.edu.vn>
2022-12-06 20:19:46 +0700

    fix(admin): fix change admin password, enhance password encryption

--- Web_hcmut/admin/admin/profile.php
+++ Web_hcmut/admin/admin/profile.php
@@ -1,97 +1,85 @@
-
-  <?php
- session_start();
+<?php
+session_start();
 include("./includes/db.php");
-if (isset($_POST['re_password']))
-  {
-    $email=$_SESSION['admin_email'];
 
-  $old_pass = $_POST['old_pass'];
-  $op = md5($old_pass);
-  $new_pass = $_POST['new_pass'];
-  $re_pass = $_POST['re_pass'];
-  $password_query = mysqli_query($con,"select * from admin_info where admin_email='$email'");
-  $password_row = mysqli_fetch_array($password_query);
-  $database_password = $password_row['admin_password'];
-  if ($database_password == $op)
-    {
-    if ($new_pass == $re_pass)
-      {
-        $pass = md5($re_pass);
-      $update_pwd = mysqli_query($con,"UPDATE admin_info set admin_password='$pass' where admin_id='6'");
-      echo "<script>alert('Update Sucessfully'); </script>";
-      }
-      else
-      {
-      echo "<script>alert('Your new and Retype Password is not match'); </script>";
-      }
+
+if (isset($_POST['re_password'])) {
+    $email = $_SESSION['admin_email'];
+    $old_pass = htmlspecialchars($_POST['old_pass']);
+    $new_pass = htmlspecialchars($_POST['new_pass']);
+    $re_pass = htmlspecialchars($_POST['re_pass']);
+    $password_query = mysqli_query($con, "SELECT admin_password FROM admin_info WHERE admin_email='$email'");
+
+    $password_row = mysqli_fetch_assoc($password_query);
+    $database_password = $password_row['admin_password'];
+
+    if (password_verify($old_pass, $database_password)) {
+        if ($new_pass == $re_pass) {
+            $password = password_hash($new_pass, PASSWORD_DEFAULT);
+            $update_password = mysqli_query($con, "UPDATE admin_info SET admin_password='$password' WHERE admin_email='$email'");
+            echo "<script>alert('Update Sucessfully'); </script>";
+        } else {
+            echo "<script>alert('Your new and Retype Password is not match'); </script>";
+        }
+    } else {
+        echo "<script>alert('Your old password is wrong'); </script>";
     }
-    else
-    {
-    echo "<script>alert('Your old password is wrong'); </script>";
-    }
-  }
- 
+}
+
 include "sidenav.php";
 include "topheader.php";
 
 ?>
-      <!-- End Navbar -->
-   <div class="content">
-        <div class="container-fluid">
-          <div class="row">
+<!-- End Navbar -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
             <div class="col-md-8">
-              <div class="card">
-                <div class="card-header card-header-primary">
-                  <h4 class="card-title">Edit Profile</h4>
-                  <p class="card-category">Complete your profile</p>
+                <div class="card">
+                    <div class="card-header card-header-primary">
+                        <h4 class="card-title">Edit Profile</h4>
+                        <p class="card-category">Complete your profile</p>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" action="profile.php">
+                            <div class="row">
+                                <div class="col-md-5">
+                                    <div class="form-group bmd-form-group">
+                                        <label class="bmd-label-floating">
+                                            <?php if (isset($_SESSION['admin_name'])) : ?><?php echo $_SESSION['admin_name']; ?>
+                                        <?php endif ?>
+
+                                        </label>
+                                        <input type="text" class="form-control" disabled="">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group bmd-form-group">
+                                        <input type="text" class="form-control" name="old_pass" id="npwd" placeholder="Enter Old Password">
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-4">
+                                    <div class="form-group bmd-form-group">
+                                        <input type="text" class="form-control" name="new_pass" id="npwd" placeholder="Change Password Here">
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="form-group bmd-form-group">
+                                        <input type="text" class="form-control" name="re_pass" id="npwd" placeholder="Confirm Password Here">
+                                    </div>
+                                </div>
+
+                                <button class="btn btn-primary pull-right" type="submit" name="re_password">Update Profile</button>
+
+                                <div class="clearfix"></div>
+                        </form>
+                    </div>
                 </div>
-                <div class="card-body">
-                  <form method="post" action="profile.php">
-                    <div class="row">
-                      <div class="col-md-5">
-                        <div class="form-group bmd-form-group">
-                          <label class="bmd-label-floating">
-                            <?php  if (isset($_SESSION['admin_name'])) : ?><?php echo $_SESSION['admin_name']; ?>
-             <?php endif ?>
-                          
-                        </label>
-                          <input type="text" class="form-control" disabled="">
-                        </div>
-                      </div>
-                     <div class="col-md-4">
-                        <div class="form-group bmd-form-group">
-                          <label class="bmd-label-floating">enter old password</label>
-                          <input type="text" class="form-control" name="old_pass" id="npwd">
-                        </div>
-                      </div>
-                    
-                  
-                      <div class="col-md-4">
-                        <div class="form-group bmd-form-group">
-                          <label class="bmd-label-floating">Change Password Here</label>
-                          <input type="text" class="form-control" name="new_pass" id="npwd">
-                        </div>
-                      </div>
-                     
-                      <div class="col-md-4">
-                        <div class="form-group bmd-form-group">
-                          <label class="bmd-label-floating">confirm Password Here</label>
-                          <input type="text" class="form-control" name="re_pass" id="npwd">
-                        </div>
-                      </div>
-               
-                    <button class="btn btn-primary pull-right" type="submit" name="re_password">Update Profile</button>
-                   
-                    <div class="clearfix"></div>
-                  </form>
-                </div>
-              </div>
             </div>
-         
-          </div>
+
         </div>
-      </div>
-      <?php
-include "footer.php";
-?>
+    </div>
+</div>

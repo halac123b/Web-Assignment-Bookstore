40d0eb80494b204376fb6951ff1c64b3d5dcd186
bravo680git <iambravo680@gmail.com>
2022-12-07 18:05:38 +0700

    toggle admin sidebar in small screen

--- Web_hcmut/admin/admin/sidenav.php
+++ Web_hcmut/admin/admin/sidenav.php
@@ -21,29 +21,26 @@ if (isset($_GET['logout'])) {
     <title>
         TechShop|Admin
     </title>
-    <meta
-        content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
-        name='viewport' />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
     <!--     Fonts and icons     -->
-    <link rel="stylesheet" type="text/css"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-    <link rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- CSS Files -->
     <link href="../assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
     <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="../assets/vendor/jquery/jquery.min.js" defer></script>
     <script src="../assets/js/script.js" defer></script>
     <style>
-    select option {
-        color: black;
-    }
+        select option {
+            color: black;
+        }
     </style>
 </head>
 
 <body class="dark-edition">
     <div class="wrapper ">
-        <div class="sidebar" data-color="purple" data-background-color="black"
-            data-image="../assets/img/sidebar-2.jpg">
+        <div class="sidebar" data-color="purple" data-background-color="black" data-image="../assets/img/sidebar-2.jpg">
             <!--
         Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
@@ -88,8 +85,7 @@ if (isset($_GET['logout'])) {
                     </li>
                     <li class="nav-item ">
                         <a class="nav-link" href="profile.php">
-                            <icons-image _ngcontent-aye-c22="" _nghost-aye-c19=""><i _ngcontent-aye-c19=""
-                                    class="material-icons icon-image-preview">settings</i></icons-image>
+                            <icons-image _ngcontent-aye-c22="" _nghost-aye-c19=""><i _ngcontent-aye-c19="" class="material-icons icon-image-preview">settings</i></icons-image>
                             <p>setting</p>
                         </a>
                     </li>
--- Web_hcmut/admin/assets/js/script.js
+++ Web_hcmut/admin/assets/js/script.js
@@ -1,13 +1,24 @@
-activeSidebarItem();
+$(document).ready(function () {
+  activeSidebarItem();
 
-function activeSidebarItem() {
-  const path =
-    location.pathname.slice(1).split("/").reverse()[0] || "index.php";
-  document.querySelectorAll(".nav-link").forEach((item) => {
-    if (item.getAttribute("href") === path) {
-      item.parentElement.classList.add("active");
-    } else {
-      item.parentElement.classList.remove("active");
-    }
+  function activeSidebarItem() {
+    const path =
+      location.pathname.slice(1).split("/").reverse()[0] || "index.php";
+    document.querySelectorAll(".nav-link").forEach((item) => {
+      if (item.getAttribute("href") === path) {
+        item.parentElement.classList.add("active");
+      } else {
+        item.parentElement.classList.remove("active");
+      }
+    });
+  }
+
+  $("body").delegate(".navbar-toggler", "click", function (event) {
+    event.stopPropagation();
+    $(".sidebar").addClass("active");
   });
-}
+
+  $(document).click(function () {
+    $(".sidebar").removeClass("active");
+  });
+});

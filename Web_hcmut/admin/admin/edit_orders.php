9426e79f11993efb87a446cb08edb2e4ec27a73b
bravo680git <iambravo680@gmail.com>
2022-12-07 16:56:30 +0700

    update table field in edit_order.php

--- Web_hcmut/admin/admin/edit_orders.php
+++ Web_hcmut/admin/admin/edit_orders.php
@@ -6,14 +6,14 @@ $action = $_GET['action'];
 
 if (!empty($order_id) &&  !empty($action)) {
     if ($action == 'complete') {
-        $query = "UPDATE orders SET p_status = 'Complete' WHERE order_id = '$order_id'";
+        $query = "UPDATE orders_info SET status = 'Complete' WHERE order_id = '$order_id'";
         $result = mysqli_query($con, $query);
         if ($result) {
             echo "<script>alert('Order Completed Successfully')</script>";
         }
         header("Location: ./orders.php");
     } else if ($action == 'cancel') {
-        $query = "UPDATE orders SET p_status = 'Cancelled' WHERE order_id = '$order_id'";
+        $query = "UPDATE orders_info SET status = 'Cancelled' WHERE order_id = '$order_id'";
         $result = mysqli_query($con, $query);
         if ($result) {
             echo "<script>alert('Order Confirmed Successfully')</script>";

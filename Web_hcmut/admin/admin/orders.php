df09c42e7a8679a22bf56b45746ea79b7306e008
bravo680git <iambravo680@gmail.com>
2022-12-07 16:52:16 +0700

    update db, fix orders.php in admin

--- Web_hcmut/admin/admin/orders.php
+++ Web_hcmut/admin/admin/orders.php
@@ -25,78 +25,96 @@ include "topheader.php";
 ?>
 <!-- End Navbar -->
 <div class="content">
-    <div class="container-fluid">
-        <!-- your content here -->
-        <div class="col-md-14">
-            <div class="card ">
-                <div class="card-header card-header-primary">
-                    <h4 class="card-title">Orders / Page <?php echo $page; ?> </h4>
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive ps">
-                        <table class="table table-hover tablesorter " id="">
-                            <thead class=" text-primary">
-                                <tr>
-                                    <th>Customer Name</th>
-                                    <th>Products</th>
-                                    <th>Quantity</th>
-                                    <th>Date</th>
-                                    <th>Contact | Email</th>
-                                    <th>Address</th>
-                                    <th>Price</th>
-                                    <th>Status</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                $result = mysqli_query($con, "SELECT op.order_id,qty,f_name,product_title,email,address,total,status,date FROM order_products AS op LEFT JOIN orders_info AS oi ON op.order_id=oi.order_id INNER JOIN products AS p ON p.product_id = op.product_id LIMIT $page1,10") or die("query 1 incorrect.....");
-
-                while ($row = mysqli_fetch_assoc($result)) {
-                  echo "<tr>
-                  <td>$row[f_name]</td>
-                  <td>$row[product_title]</td>
-                  <td>$row[qty]</td>
-                  <td>$row[date]</td>
-                  <td>$row[email]</td>
-                  <td>$row[address]</td>
-                  <td>$row[total]</td>";
-                  if ($row["status"] == 'Delivering') {
-                    echo "<td class='text-secondary'>$row[status]</td>";
-                  } else if ($row["status"] == 'Cancelled') {
-                    echo "<td class='text-danger'>$row[status]</td>";
-                  } else {
-                    echo "<td class='text-success'>$row[status]</td>";
-                  }
-
-                  if ($row["status"] == 'Delivering') {
-                    echo "
-                  <td>
-                        <a class='btn btn-sm btn-danger' href='edit_orders.php?order_id=$row[order_id]&action=cancel'>Cancel</a>
-                        <a class='btn btn-sm btn-success' href='edit_orders.php?order_id=$row[order_id]&action=complete'>Complete</a>
-                  </td>
-                      ";
-                  }
-                  echo "</tr>";
-                }
-                ?>
-                            </tbody>
-                        </table>
-
-                        <div class="ps__rail-x" style="left: 0px; bottom: 0px;">
-                            <div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>
-                        </div>
-                        <div class="ps__rail-y" style="top: 0px; right: 0px;">
-                            <div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="container-fluid">
+    <!-- your content here -->
+    <div class="col-md-14">
+      <div class="card ">
+        <div class="card-header card-header-primary">
+          <h4 class="card-title">Orders / Page <?php echo $page; ?> </h4>
         </div>
+        <div class="card-body">
+          <div class="table-responsive ps">
+            <table class="table table-hover tablesorter " id="">
+              <thead class=" text-primary">
+                <tr>
+                  <th>Customer Name</th>
+                  <th>Products</th>
+                  <th>Quantity</th>
+                  <th>Date</th>
+                  <th>Contact | Email</th>
+                  <th>Address</th>
+                  <th>Price</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                $result = mysqli_query($con, "SELECT op.order_id,qty,f_name,product_title,email,address,total,status,date FROM order_products AS op LEFT JOIN orders_info AS oi ON op.order_id=oi.order_id INNER JOIN products AS p ON p.product_id = op.product_id LIMIT $page1,10") or die("query 1 incorrect.....");
+                $prev_order_id = -1;
+                while ($row = mysqli_fetch_assoc($result)) {
+                  $order_id = $row['order_id'];
+                  $count = mysqli_num_rows(mysqli_query($con, "SELECT order_id FROM order_products WHERE order_id=$order_id"));
+                  if ($order_id != $prev_order_id) {
+                    echo "
+                        <tr>
+                          <td rowspan='$count'>$row[f_name]</td>
+                    ";
+                  } else {
+                    echo "
+                        <tr>
+                    ";
+                  }
+                  echo "
+                      <td>$row[product_title]</td>
+                      <td>$row[qty]</td>
+                    ";
+
+                  if ($order_id != $prev_order_id) {
+                    $prev_order_id = $order_id;
+                    echo "
+                        <td rowspan='$count'>$row[date]</td>
+                        <td rowspan='$count'>$row[email]</td>
+                        <td rowspan='$count'>$row[address]</td>
+                        <td rowspan='$count'>$row[total]</td>
+                      ";
+                    if ($row["status"] == 'Delivering') {
+                      echo "<td rowspan='$count' class='text-secondary'>$row[status]</td>";
+                    } else if ($row["status"] == 'Cancelled') {
+                      echo "<td rowspan='$count' class='text-danger'>$row[status]</td>";
+                    } else {
+                      echo "<td rowspan='$count' class='text-success'>$row[status]</td>";
+                    }
+
+                    if ($row["status"] == 'Delivering') {
+                      echo "
+                          <td rowspan='$count'>
+                                <a class='btn btn-sm btn-danger' href='edit_orders.php?order_id=$row[order_id]&action=cancel'>Cancel</a>
+                                <a class='btn btn-sm btn-success' href='edit_orders.php?order_id=$row[order_id]&action=complete'>Complete</a>
+                          </td>
+                              ";
+                    }
+                  }
+                }
+                echo "</tr>";
+                ?>
+              </tbody>
+            </table>
+
+            <div class="ps__rail-x" style="left: 0px; bottom: 0px;">
+              <div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>
+            </div>
+            <div class="ps__rail-y" style="top: 0px; right: 0px;">
+              <div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
 <script>
-$(document).ready(function() {
+  $(document).ready(function() {
     $('#orders').addClass('active');
-});
+  });
 </script>
